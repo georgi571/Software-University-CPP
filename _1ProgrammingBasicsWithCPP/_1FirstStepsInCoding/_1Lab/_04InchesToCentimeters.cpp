5ba12449412d9c0c8efe5eeb4dc2312135f10ea2
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-07 18:57:44 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_1FirstStepsInCoding/_1Lab/_04InchesToCentimeters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double inch;
+    cin >> inch;
+
+    double centimeters = inch * 2.54;
+    cout << centimeters << endl;
+    return 0;
+}
