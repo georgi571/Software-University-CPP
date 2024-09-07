a0431156ab2463b773b403b9a4d7f0349347c538
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-07 18:57:30 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_1FirstStepsInCoding/_1Lab/_03RectangleArea.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    int b;
+    cin >> b;
+
+    int rectangleArea = a * b;
+    cout << rectangleArea << endl;
+    return 0;
+}
