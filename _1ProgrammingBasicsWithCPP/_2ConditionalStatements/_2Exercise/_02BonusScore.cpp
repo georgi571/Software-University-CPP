d2cf0105bdef4c8ee5519b31ce11e16ae283ed1c
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-15 23:59:23 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_2ConditionalStatements/_2Exercise/_02BonusScore.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+
+    double bonus = 0;
+    if (number <= 100) {
+        bonus += 5;
+    } else if (number <= 1000) {
+        bonus += number * 0.20;
+    } else if (number > 1000) {
+        bonus += number * 0.10;
+    }
+
+    if (number % 2 == 0) {
+        bonus += 1;
+    } else if (number % 10 == 5) {
+        bonus += 2;
+    }
+
+    double numberPlusBonus = number + bonus;
+
+    cout << bonus << endl;
+    cout << numberPlusBonus << endl;
+
+    return 0;
+}
