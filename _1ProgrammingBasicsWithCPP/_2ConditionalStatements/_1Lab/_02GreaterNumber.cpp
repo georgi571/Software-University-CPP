5faef16e655077eae4897192807c177f1996dfd2
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-15 23:18:21 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_2ConditionalStatements/_1Lab/_02GreaterNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1;
+    cin >> number1;
+
+    int number2;
+    cin >> number2;
+
+    if (number1 > number2) {
+        cout << number1 << endl;
+    } else{
+        cout << number2 << endl;
+    }
+    return 0;
+}
