efac9193724e682c00baa8065760e240249c6ae6
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-22 10:08:40 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_3ConditionalStatementsAdvanced/_1Lab/_10InvalidNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+
+    string output;
+    if ((number < 100 || number > 200) && number != 0) {
+        output = "invalid";
+    }
+
+    cout << output << endl;
+
+    return 0;
+}
