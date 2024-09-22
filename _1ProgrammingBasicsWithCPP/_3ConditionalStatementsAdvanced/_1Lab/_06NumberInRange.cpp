29a444a46d03937be031bbe9e34af4a03dfab3b3
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-22 09:48:47 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_3ConditionalStatementsAdvanced/_1Lab/_06NumberInRange.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+
+    string output;
+    if (number >= -100 && number <= 100 && number != 0) {
+        output = "Yes";
+    } else {
+        output = "No";
+    }
+
+    cout << output << endl;
+
+    return 0;
+}
