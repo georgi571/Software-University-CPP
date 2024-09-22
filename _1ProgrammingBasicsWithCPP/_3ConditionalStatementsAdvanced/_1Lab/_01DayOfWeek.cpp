fe72acf2c55371516ba14a5e195e5d6686d34349
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-22 09:22:23 +0300

    add solution

---
+++ _1ProgrammingBasicsWithCPP/_3ConditionalStatementsAdvanced/_1Lab/_01DayOfWeek.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+
+    string day;
+
+    switch (number) {
+        case 1:
+            day = "Monday";
+        break;
+        case 2:
+            day = "Tuesday";
+        break;
+        case 3:
+            day = "Wednesday";
+        break;
+        case 4:
+            day = "Thursday";
+        break;
+        case 5:
+            day = "Friday";
+        break;
+        case 6:
+            day = "Saturday";
+        break;
+        case 7:
+            day = "Sunday";
+        break;
+        default:
+            day = "Error";
+        break;
+    }
+
+    cout << day << endl;
+
+    return 0;
+}
