8fdfa3e327d0d3e6c9bf4e50614211a4d02e19a7
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-29 19:33:26 +0300

    add solution

--- _1ProgrammingBasicsWithCPP/_4ForLoop/_1Lab/_10OddEvenSum.cpp
+++ _1ProgrammingBasicsWithCPP/_4ForLoop/_1Lab/_10OddEvenSum.cpp
@@ -1,3 +1,33 @@
-//
-// Created by Gamer on 9/29/2024.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int odd = 0;
+    int even = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int number;
+        cin >> number;
+
+        if (i % 2 == 0) {
+            odd += number;
+        } else {
+            even += number;
+        }
+    }
+
+    int diff = abs(odd - even);
+
+    if (odd == even) {
+        cout << "Yes" << endl;
+        cout << "Sum = " << odd << endl;
+    } else {
+        cout << "No" << endl;
+        cout << "Diff = " << diff << endl;
+    }
+
+    return 0;
+}
