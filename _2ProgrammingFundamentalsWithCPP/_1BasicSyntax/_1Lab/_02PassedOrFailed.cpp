be3d33bc04e1df8e64c70df688b9687a6d3b82e9
Georgi Kostadinov <georgi93571@gmail.com>
2024-11-11 18:17:28 +0200

    add solution

---
+++ _2ProgrammingFundamentalsWithCPP/_1BasicSyntax/_1Lab/_02PassedOrFailed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double grade;
+    cin >> grade;
+
+    if (grade >= 3) {
+        cout << "Passed!" << endl;
+    } else {
+        cout << "Failed!" << endl;
+    }
+
+    return 0;
+}
