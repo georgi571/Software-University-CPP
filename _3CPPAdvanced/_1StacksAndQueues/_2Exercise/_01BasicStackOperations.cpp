221e023392aed721373d8088f3856ddc6de17def
Georgi Kostadinov <georgi93571@gmail.com>
2025-01-28 21:48:16 +0200

    add solution

---
+++ _3CPPAdvanced/_1StacksAndQueues/_2Exercise/_01BasicStackOperations.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <climits>
+using namespace std;
+
+int main() {
+    string input;
+    getline(cin, input);
+
+    istringstream iss(input);
+
+    int n, s, x;
+    iss >> n >> s >> x;
+
+    stack<int> numbers;
+
+    for (int i = 0; i < n; ++i) {
+        int number;
+        cin >> number;
+        numbers.push(number);
+    }
+
+    for (int i = 0; i < s; ++i) {
+        numbers.pop();
+    }
+
+    if (numbers.empty()) {
+        cout << "0" << endl;
+    } else {
+        bool flag = false;
+        int smallest = INT_MAX;
+        while (!numbers.empty()) {
+            int number = numbers.top();
+            numbers.pop();
+            if (number < smallest) {
+                smallest = number;
+            }
+            if (number == x) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            cout << "true" << endl;
+        } else {
+            cout << smallest << endl;
+        }
+    }
+
+    return 0;
+}
